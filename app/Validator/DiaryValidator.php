ce71d0445f592ae0dbcebaacdb9448d69625c01f
vgualdron <victor.gualdron.r@gmail.com>
2025-01-10 03:00:30 -0500

    asda

--- app/Validator/DiaryValidator.php
+++ app/Validator/DiaryValidator.php
@@ -9,19 +9,19 @@
         public function validate($data, $id){
             $this->data = $data;
             $this->data['id'] = $id;
-            return Validator::make($this->data, $this->rules(), $this->messages());            
+            return Validator::make($this->data, $this->rules(), $this->messages());
         }
 
         private function rules(){
-            return[                             
-                'userId' => 'required',
+            return[
+                'user_id' => 'required',
                 'date' => 'required',
             ];
         }
 
         private function messages(){
             return [
-                'userId.required' => 'El id de usuario es requerido',
+                'user_id.required' => 'El id de usuario es requerido',
                 'date.required' => 'La fecha es requerida',
             ];
         }
