07c56edfa9d819df3d34ec31cbe56b3440f9fdba
vgualdron <victor.gualdron.r@gmail.com>
2025-01-13 13:29:24 -0500

    change range on red days

--- app/Services/Implementations/NovelServiceImplement.php
+++ app/Services/Implementations/NovelServiceImplement.php
@@ -296,7 +296,7 @@
                 $sql .= " GROUP BY
                         lendings.id, news.id, districts.id, address_data.address_type, address_data.address, address_data.district
                     HAVING
-                        days_since_creation > 22
+                        days_since_creation > 21
                     ORDER BY
                         CAST(SUBSTRING_INDEX(districts.order, ' ', 1) AS UNSIGNED) ASC,
                         SUBSTRING_INDEX(districts.order, ' ', -1) ASC,
