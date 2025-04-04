204a6c6140bb06bb43ed49bfc4f9bf72f887dbe3
vgualdron <victor.gualdron.r@gmail.com>
2025-04-04 18:41:06 -0500

    add Loans

--- config/filesystems.php
+++ config/filesystems.php
@@ -90,6 +90,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'deposits' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/deposits'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
